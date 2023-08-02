19bae1b89d713c46eb3efbb15d677d4bc86c2f83
Vijaya Amodkar <140308552+VijayaAmodkar@users.noreply.github.com>
2023-08-02 14:11:54 +0530

    By Vijaya

---
+++ 3_Shop.js
@@ -0,0 +1,17 @@
+/* Q.3] A shop will give discount of 10% if the total cost of purchased quantity is more than 1000. Ask user for quantity & assume per unit cost to be 100. Judge and print total cost for user. */
+let value, tprice, cost=100;
+let qty = prompt("Enter qty purchased");
+qty = Number.parseInt(qty);
+tprice = qty*cost;
+if (tprice>=1000)
+{
+  value=tprice*0.1; //calculates 10% of value in "tprice"
+  console.log("\n Discount allowed = %d", value);
+  console.log("\n Total price without discount deducted = %d", tprice);
+  console.log("\n Grand Total price = %d", tprice-value);
+}
+else
+{
+  console.log("\n No Discount allowed");
+  console.log("\n Total price without discount deducted = %d", tprice);
+}
